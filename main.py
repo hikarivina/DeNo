df190468482c29f78d1c24d1b0de6f7b0e03b63a
Hikarivina <dangnguyenhuu88@gmail.com>
2020-12-13 17:29:24 +0900

    check chain backward

--- main.py
+++ main.py
@@ -40,12 +40,25 @@ a = A(x)
 b = B(a)
 y = C(b)
 
-assert y.creator == C
-assert y.creator.input == b
-assert y.creator.input.creator == B
-assert y.creator.input.creator.input == a
-assert y.creator.input.creator.input.creator == A
-assert y.creator.input.creator.input.creator.input == x
+y. grad = np.array(1.0)
+C = y.creator
+b = C.input
+b.grad = C.backward(y.grad)
+B = b.creator
+a = B.input
+a.grad = B.backward(b.grad)
+A = a.creator
+x = A.input
+x.grad = A.backward(a.grad)
+
+print(x.grad)
+
+# assert y.creator == C
+# assert y.creator.input == b
+# assert y.creator.input.creator == B
+# assert y.creator.input.creator.input == a
+# assert y.creator.input.creator.input.creator == A
+# assert y.creator.input.creator.input.creator.input == x
 
 # def f(x):
 #     A = Square()
