29547f3009a018a09d2f6bb1c2a3be9d60525576
Hikarivina <dangnguyenhuu88@gmail.com>
2020-12-14 00:46:26 +0900

    clear git cache

---
+++ Variable.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
