7d245ea6cca9284977dc629c97e83a4049b14c9e
Hikarivina <dangnguyenhuu88@gmail.com>
2020-12-13 17:52:48 +0900

    auto backpropagation

--- main.py
+++ main.py
@@ -40,16 +40,19 @@ a = A(x)
 b = B(a)
 y = C(b)
 
-y. grad = np.array(1.0)
-C = y.creator
-b = C.input
-b.grad = C.backward(y.grad)
-B = b.creator
-a = B.input
-a.grad = B.backward(b.grad)
-A = a.creator
-x = A.input
-x.grad = A.backward(a.grad)
+# y. grad = np.array(1.0)
+# C = y.creator
+# b = C.input
+# b.grad = C.backward(y.grad)
+# B = b.creator
+# a = B.input
+# a.grad = B.backward(b.grad)
+# A = a.creator
+# x = A.input
+# x.grad = A.backward(a.grad)
+
+y.grad = np.array(1.0)
+y.backward()
 
 print(x.grad)
 
