570fcd60a43392e8329cb37d52172bdd02ac6842
Hikarivina <dangnguyenhuu88@gmail.com>
2020-12-13 16:39:30 +0900

    implement backpropagation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Variable:
 
 class Function:
     def __call__(self, input):
+        self.input = input
         x = input.data
         y = self.forward(x)
         output = Variable(y)
@@ -16,13 +17,26 @@ class Function:
     def forward(self, x):
         raise NotImplementedError()
 
+    def backward(self):
+        raise NotImplementedError()
+
 class Square(Function):
     def forward(self, x):
         return x ** 2
 
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 
 def numerical_diff(f, x, eps=1e-4):
@@ -35,16 +49,16 @@ def numerical_diff(f, x, eps=1e-4):
     return (y1.data - y0.data) / (2 * eps)
 
 
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
+# def f(x):
+#     A = Square()
+#     B = Exp()
+#     C = Square()
 
-    return C(B(A(x)))
+#     return C(B(A(x)))
 
-x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-print(dy)
+# x = Variable(np.array(0.5))
+# dy = numerical_diff(f, x)
+# print(dy)
 
 # f = Square()
 # x = Variable(np.array(2))
@@ -52,14 +66,20 @@ print(dy)
 # dy = numerical_diff(f, x)
 # print(dy)
 
-# A = Square()
-# B = Exp()
-# C = Square()
+A = Square()
+B = Exp()
+C = Square()
 
-# x = Variable(np.array(0.5))
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
 
-# a = A(x)
-# b = B(a)
-# y = C(b)
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+
+print(x.grad)
 
 # print(y.data)
